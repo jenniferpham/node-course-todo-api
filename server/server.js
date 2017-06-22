0927bafdd7f22097791058bea8a89eb09263f069
Jennifer Le Pham <jennifer.pham@glidewelldental.com>
2017-06-21 19:29:09 -0700

    adding getting specific todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //EXTERNAL
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 //INTERNAL
 var {mongoose} = require ('./db/mongoose'); //can leave of .js extension. creates a mongoose variable same as variable from the file required
@@ -29,6 +30,29 @@ app.get('/todos', (req, res) => {
         res.send({todos}) //seding object is much more flexible
     }, (e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    //check if id is valid using isValid method. if not, give 404 and send back empty body
+    //findById
+        //success
+            //if todo - send it back
+            //if no todo - send back 404 with empty body
+        //error
+            //400 - and send empty body back
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send({});
+    }
+
+    Todo.findById(id).then( (result)=> {
+        if(!result){ //it is a valid id but just not in our collection
+            res.status(404).send({})
+        }
+        res.status(200).send({todo: result});
+    }).catch((err) => {
+        res.status(400).send({});  //this would happen when the id is not valid
     })
 })
 
