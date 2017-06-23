d6988a696b63b625f0929f906ad5ba96113e3154
Jennifer Le Pham <jennifer.pham@glidewelldental.com>
2017-06-22 20:54:23 -0700

    bcrypt

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,35 @@
 const {SHA256} = require('crypto-js');  //256 is # of bit
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+//hash and salt password before they are saved
+
+bcrypt.genSalt(10, (err, salt) => { //# of rounds to generate salt (more takes longer but less chance of brute force), 2nd arg is callback
+    bcrypt.hash(password, salt, (err, hash) => {
+        // console.log(hash);
+    });
+} );  
+
+var hashedPassword = '$2a$10$ZfrV9pN7c5dXWtTWUx7Ao.SUpnqJnKiyd.kt5jBT9W/rg6UG4ESca';
+
+bcrypt.compare(password, hashedPassword, (err, result) =>{
+    console.log(result);
+});
 
 // jwt.sign - creates the hash and returns the token value
 // jwt.verify - makes sure data was not manipulated
 
-var data = {
-    id: 10
-}
+// var data = {
+//     id: 10
+// }
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
 
-var decoded = jwt.verify(token, '123abc'); //2nd argument is the secret. must be same as the token. only returns decoded value if it's the same secret. if not, it will throw an error.
-console.log(decoded);
+// var decoded = jwt.verify(token, '123abc'); //2nd argument is the secret. must be same as the token. only returns decoded value if it's the same secret. if not, it will throw an error.
+// console.log(decoded);
 
 
 
